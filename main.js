d32aa33e50a9137aa6afca9c53f265a5ba440b30
danielson <caosdan@gmail.com>
2021-08-21 13:59:08 -0300

    Player movendo automaticamente

--- main.js
+++ main.js
@@ -1,23 +1,50 @@
 
 const cnv = document.querySelector('#cnv');
+const ctx = document.querySelector('#cnv').getContext('2d');
+
+const player = {
+    x: 400,
+    y: 400,
+    width: 48,
+    height: 48,
+
+    update(){
+        this.x++;
+    },
+
+    render(){
+        
+        ctx.fillStyle= '#0f0';
+        ctx.fillRect(this.x, this.y,this.width, this.height);
+        
+    }
+}
 
 function init(){
     cnv.width = '900';
     cnv.height = '800';
     cnv.style.backgroundColor = '#555';
+
 }
 
 function update(){
-    
+    player.update();
 }
 
 function render(){
+    ctx.fillStyle= '#555';
+    ctx.fillRect(0, 0,cnv.width, cnv.height);
+    
+    
+    player.render();
 
 }
 
 function loop(){
     update();
+    
     render();
+    
     requestAnimationFrame(loop);
 }
 
